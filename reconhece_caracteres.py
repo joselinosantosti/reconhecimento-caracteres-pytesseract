22ad397f6d879790ecec1e835482c1378870753e
Joselino Santos <joselinosantosti@gmail.com>
2018-12-31 19:46:04 -0200

    Update reconhece_caracteres.py

--- reconhece_caracteres.py
+++ reconhece_caracteres.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import pytesseract
 
-img = Image.open("img/placa4.jpg")
+img = Image.open("imagens/comando1.jpg")
 text = pytesseract.image_to_string(img, lang='eng')
 print(text)
